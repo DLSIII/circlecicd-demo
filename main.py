8ae4761ae9a9ac50b783c06d18c5b3e6b001e3c6
DLSIII <159146136+DLSIII@users.noreply.github.com>
2024-04-17 10:30:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def Add(a, b):
-        return a + b
+        return a * b
         
 def SayHello():
-        print("Hello CIS 411")
+        print("Hello CIS 411 - System Analysis and Design")
+        print("Test")
 
 if __name__ == '__main__':
         SayHello()
